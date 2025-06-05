cac218b7aa82f05a2227acb4a444255e6d0ada7e
Pierce Lin <a0916083121@gmail.com>
2025-06-06 01:02:25 +0800

    refactor(RegisterPage): 新增確認密碼欄位
    1. 註冊帳戶表單新增確認密碼欄位
        * 在 `RegisterPage` 新增確認密碼欄位
        * 在 `auth.schema` 中新增密碼與確認密碼是否相符的驗證邏輯
    2. 調整 `LoginPage` 與 `RegisterPage` 的內容
        * 在表單底部加上「尚未有帳號」以及「已經有帳號」的連結，讓使用者在登入與註冊頁面間能來回導航
        * 將表單文字調整成成繁體中文
    3. 將 `api` 中的環境變數改為 zod 驗證過的 env

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -6,20 +6,23 @@ import { useAuth } from "@/store/auth/useAuth";
 import { registerSchema, type RegisterSchemaType } from "@convo/shared";
 import { AxiosError } from "axios";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { z } from "zod/v4";
 
 const defaultError = {
 	username: undefined,
-	password: undefined,
 	email: undefined,
+	password: undefined,
+	comfirmPassword: undefined,
 };
 
 export default function RegisterPage() {
 	const [loading, setLoading] = useState(false);
 	const [formInput, setFormInput] = useState<RegisterSchemaType>({
 		username: "",
-		password: "",
 		email: "",
+		password: "",
+		comfirmPassword: "",
 	});
 	const [errors, setErrors] =
 		useState<Partial<RegisterSchemaType>>(defaultError);
@@ -47,6 +50,8 @@ export default function RegisterPage() {
 					username: formErrors.username && formErrors.username[0],
 					password: formErrors.password && formErrors.password[0],
 					email: formErrors.email && formErrors.email[0],
+					comfirmPassword:
+						formErrors.comfirmPassword && formErrors.comfirmPassword[0],
 				});
 				setApiError(null);
 				return;
@@ -73,16 +78,16 @@ export default function RegisterPage() {
 	return (
 		<div className="m-auto w-2/3 max-w-[500px] space-y-14">
 			<div>
-				<h1 className="text-title text-center">Sign Up And Chat</h1>
+				<h1 className="text-title text-center">註冊帳戶並開始聊天</h1>
 				<p className="text-description text-center">
-					Enter your name, password, and email to sign up and chat.
+					輸入名稱、電子信箱以及密碼，開始與人線上聊天吧
 				</p>
 			</div>
 			<form className="space-y-6" onSubmit={handleSubmit}>
 				<FormInput
 					id="username"
 					name="username"
-					label="username"
+					label="使用者名稱"
 					placeholder="eg. John"
 					onChange={handleInputChange}
 					errorMessage={errors.username}
@@ -90,7 +95,7 @@ export default function RegisterPage() {
 				<FormInput
 					id="email"
 					name="email"
-					label="email"
+					label="電子信箱"
 					placeholder="eg. john1234@gmail.com"
 					onChange={handleInputChange}
 					errorMessage={errors.email}
@@ -98,15 +103,33 @@ export default function RegisterPage() {
 				<FormInput
 					id="password"
 					name="password"
-					label="password"
-					placeholder="Enter your password"
+					label="密碼"
+					type="password"
+					placeholder="輸入你的密碼"
 					onChange={handleInputChange}
 					errorMessage={errors.password}
 				/>
+				<FormInput
+					id="comfirmPassword"
+					name="comfirmPassword"
+					label="確認密碼"
+					type="password"
+					placeholder="再輸入一次密碼"
+					onChange={handleInputChange}
+					errorMessage={errors.comfirmPassword}
+				/>
 				<Button type="submit" disabled={loading}>
-					Log In
+					註冊
 				</Button>
 				{apiError && <ResponseMessage type="error" message={apiError} />}
+				<p className="text-center text-sm text-neutral-400">
+					已經有帳號了？
+					<Link to={"/login"}>
+						<span className="hover:text-neutral-100 hover:underline">
+							點擊登入
+						</span>
+					</Link>
+				</p>
 			</form>
 		</div>
 	);
--- shared/src/schemas/auth.schema.ts
+++ shared/src/schemas/auth.schema.ts
@@ -1,5 +1,4 @@
 import { z } from "zod/v4";
-import { UserDTO } from "../types/dto.types";
 import { usernameSchema, passwordSchema, emailSchema } from "./common.schema";
 
 export const loginSchema = z.object({
@@ -9,10 +8,16 @@ export const loginSchema = z.object({
 
 export type LoginSchemaType = z.infer<typeof loginSchema>;
 
-export const registerSchema = z.object({
-	username: usernameSchema,
-	email: emailSchema,
-	password: passwordSchema,
-});
+export const registerSchema = z
+	.object({
+		username: usernameSchema,
+		email: emailSchema,
+		password: passwordSchema,
+		comfirmPassword: passwordSchema,
+	})
+	.refine((data) => data.password === data.comfirmPassword, {
+		message: "確認密碼與密碼不符",
+		path: ["comfirmPassword"],
+	});
 
 export type RegisterSchemaType = z.infer<typeof registerSchema>;
