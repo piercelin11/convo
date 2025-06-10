fa4a05e235efb7aa4e0614769355457a66710284
Pierce Lin <a0916083121@gmail.com>
2025-06-10 15:08:53 +0800

    refactor(useCreateGroup): 實現創建聊天室的樂觀更新

--- client/src/queries/chat/useCreateGroup.ts
+++ client/src/queries/chat/useCreateGroup.ts
@@ -8,21 +8,46 @@ import type {
 } from "@convo/shared";
 import { AxiosError } from "axios";
 
+type CreateGroupContext = {
+	prevResponse: ChatResponseType | undefined;
+};
+
 export default function useCreateGroup() {
 	const queryClient = useQueryClient();
 	return useMutation<
 		ChatResponseType,
 		AxiosError<ApiResponseSchemaType>,
-		CreateGroupChatSchemaType
+		CreateGroupChatSchemaType,
+		CreateGroupContext
 	>({
 		mutationFn: chatService.createGroupChat,
-		onSuccess: () => {
-			queryClient.invalidateQueries({ queryKey: chatKeys.all });
+		onMutate: (newGroup) => {
+			queryClient.cancelQueries({ queryKey: chatKeys.all });
+			const prevResponse = queryClient.getQueryData(
+				chatKeys.all
+			) as ChatResponseType;
+			queryClient.setQueryData(chatKeys.all, (old: ChatResponseType) => ({
+				...old,
+				data: [
+					...old.data,
+					{
+						id: Date.now().toString(),
+						...newGroup,
+					},
+				],
+			}));
+
+			return { prevResponse };
 		},
-		onError: (error) => {
+		onError: (error, _newGroup, context) => {
 			if (error instanceof AxiosError)
 				console.error("創建聊天室時發生錯誤:", error);
 			else console.error("創建聊天室時發生未預期的錯誤:", error);
+
+			if (context) queryClient.setQueryData(chatKeys.all, context.prevResponse);
+		},
+		onSettled: () => {
+			queryClient.invalidateQueries({ queryKey: chatKeys.all });
 		},
 	});
 }
